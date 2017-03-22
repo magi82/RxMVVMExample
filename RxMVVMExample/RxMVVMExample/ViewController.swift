ba97fd911a42085876483a2ffcfb820fd629faba
Byung Kook Hwang <bkhwang82@gmail.com>
2017-03-22 17:25:46 +0900

    init project

---
+++ RxMVVMExample/RxMVVMExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxMVVMExample
+//
+//  Created by Byung Kook Hwang on 2017. 3. 22..
+//  Copyright © 2017년 Byung Kook Hwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
